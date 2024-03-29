d445b8bb6f8c58da886b05e33d8c84b08425b80b
Orlina Joly <ojoly@et.esiea.fr>
2024-03-29 10:14:19 +0100

    Rendu TP Wordpress

---
+++ Module-07-bis/wordpress/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1712306873;s:7:"authKey";s:64:"o?Lg`,*4ygXhJiN)fb+1l.c&h2=)<j>r2zjq?)Dz5X3Uf;90l#3F&x9xm)NcX9vS";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:184;}
